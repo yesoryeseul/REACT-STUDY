a340d0d5adcecfb327a7379f684b2f120c8661e8
yesoryeseul <yesoryeseul07@gmail.com>
2023-07-04 23:25:28 +0900

    리액트 쿼리키 이해

--- react-query-ex/src/hooks/queries/use-queries.js
+++ react-query-ex/src/hooks/queries/use-queries.js
@@ -22,6 +22,7 @@ export const useGetMovies = {
 		);
 	},
 	// 하나의 로직으로 엔드포인트만 다르게 사용할 수 있다!
+	// 쿼리키에 language를 넣어 언어 변경 기능 해결
 	ListMovies: (endpoint, params, language) => {
 		return useQuery([QUERYKEYS.MOVIE_LIST, language], () =>
 			MovieApi.getMoviesList(endpoint, params, language),
@@ -29,4 +30,11 @@ export const useGetMovies = {
 	},
 };
 
-// refetch or invalidate
+/**
+ * 쿼리키에 language를 넣어 React Query 라이브러리의 캐시 기능을 활용!
+ * React Query는 데이터를 캐싱하여 중복된 요청을 최소화하고 성능을 향상시키는 기능을 제공
+ * 첫 번째 인자인 쿼리키 [QUERYKEYS.MOVIE_LIST, language] 는 영화 목록 요청의 캐시를 구분하기 위한 고유한 식별자로 사용
+ * language 넣어주어 변수의 값이 변경될 때마다 해당 쿼리키가 변경되므로 데이터를 다시 불러오는 것
+ */
+
+// refetch or invalidate 방법으로도 언어변경 가능
