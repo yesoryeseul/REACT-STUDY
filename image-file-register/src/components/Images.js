39a4105ac6d90484289652eab5a2e5fc64af84d7
yesoryeseul <yesoryeseul07@gmail.com>
2023-07-17 02:05:40 +0900

    feat: 이미지 최대 5개 로직, 레이아웃 css 추가

--- image-file-register/src/components/Images.js
+++ image-file-register/src/components/Images.js
@@ -10,12 +10,11 @@ const IamgeRegister = () => {
   };
 
   const handleOnChange = (e) => {
-    // console.log(e.target.files[0]);
     const files = e.target.files;
     const imgUrls = Array.from(files).map((imgFile) =>
       URL.createObjectURL(imgFile)
     );
-    setImageSrcList((prev) => [...prev, ...imgUrls]);
+    setImageSrcList((prev) => [...prev, ...imgUrls].slice(0, 5));
   };
   return (
     <div>
@@ -27,9 +26,28 @@ const IamgeRegister = () => {
         style={{ display: "none" }}
         multiple
       />
-      {imageSrcList.map((imageUrl, idx) => (
-        <img key={idx} src={imageUrl} alt={`uploaded image ${idx}`} />
-      ))}
+      <div
+        style={{
+          display: "grid",
+          gap: "15px",
+          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+          maxWidth: "1060px",
+          margin: "0 auto",
+        }}
+      >
+        {imageSrcList.map((imageUrl, idx) => (
+          <img
+            key={idx}
+            src={imageUrl}
+            alt={`uploaded image ${idx}`}
+            style={{
+              width: "200px",
+              height: "200px",
+              border: "1px solid #ddd",
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 };
