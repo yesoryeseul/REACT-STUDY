4d4639e47a701f9bb60fc4a2f8034008ee6c9403
yesoryeseul <yesoryeseul07@gmail.com>
2024-02-21 00:13:40 +0900

    component style apply

--- next-stitches/src/app/page.tsx
+++ next-stitches/src/app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+import { styled } from "@stitches/react";
+
+const Button = styled("button", {
+  backgroundColor: "$gray500",
+  borderRadius: "9999px",
+  border: "1px solid $transparent",
+  fontSize: "13px",
+  padding: "10px 15px",
+  "&:hover": {
+    backgroundColor: "$warn",
+  },
+  color: "$white",
+});
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button>Button</Button>
+    </>
+  );
 }
